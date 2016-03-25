be33ba3e25d94a882728079f8fff661a9e36baff
mpilcher90 <mpilcher90@gmail.com>
2016-03-25 08:19:57 +0000

    add javascript file and select elements

---
+++ calculator/js/script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+  var buttons = document.getElementsByTagName('span'),
+      result = document.querySelectorAll('.result p')[0],
+      clear = document.getElementsByClassName('clear')[0];
+};
