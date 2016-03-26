d9f2645c43b2b4b5ba170324b5912b7e758b0d6d
mpilcher90 <mpilcher90@gmail.com>
2016-03-26 07:38:28 +0000

    finish vowel counting function

--- miscellaneous.js
+++ miscellaneous.js
@@ -1,9 +1,28 @@
-function my_max(array) {
-  var max = array[0];
-  for (var i = 1, iCnt = array.length; i < iCnt; i += 1) {
-    if (array[i] > max) {
-      max = array[i];
+// Returns highest number in an array
+var my_max = function(array){
+ var max=array[0];   
+    for (var i = 0; i < array.length; i++){
+        if (array[i] > max) {
+            max=array[i];
+        }
     }
-  }
-  return max;
-}
+   return max;
+};
+
+my_max([3,4,3,5,6,80]);
+
+// Takes a string and returns the number of vowels
+
+var vowel_count = function(string) {
+   var vowels=['A','E','I','O','U','Y'];
+   var count=0;
+    for (var i = 0; i < string.length; i++){
+        if (vowels.indexOf(string[i].toUpperCase()) != -1) {
+            count ++;
+        }
+    }
+    return count;
+};
+
+
+
