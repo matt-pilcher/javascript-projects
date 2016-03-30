8f6c69af22ebe058e61507c6680beb9966bf5e25
mpilcher90 <mpilcher90@gmail.com>
2016-03-30 08:12:38 +0000

    finish screen drawing

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-		//Creates a grid of divs for the screen
-    for(var x = 0; x < 40; x++) {
+		// Creates a grid of divs for the screen
+    for(var x = 0; x < 60; x++) {
         for(var y = 0; y < 40; y++) {
             var unit = $("<div class='unit'></div>");
             unit.appendTo('#screen');
         }
     }
+    // Changes the color of the each unit when the mouse passes over
+    $('.unit').mouseenter(function() {
+    	$(this).css('background', '#666');
+    });
 });
