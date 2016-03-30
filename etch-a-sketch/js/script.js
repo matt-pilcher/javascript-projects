91877eaa9fc01bae787ab7e8087388605c16e9da
mpilcher90 <mpilcher90@gmail.com>
2016-03-30 07:44:30 +0000

    generate a grid of divs to be used for the screen

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  
+		//Creates a grid of divs for the screen
+    for(var x = 0; x < 40; x++) {
+        for(var y = 0; y < 40; y++) {
+            var unit = $("<div class='unit'></div>");
+            unit.appendTo('#screen');
+        }
+    }
 });
