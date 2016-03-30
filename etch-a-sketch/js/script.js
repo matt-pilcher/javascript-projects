2b32ee69041c778fc2f9670b650efb6bf089e200
mpilcher90 <mpilcher90@gmail.com>
2016-03-30 13:01:18 +0000

    add inset border shadow to the screen and finish clear screeen function

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-		// Creates a grid of divs for the screen
+	// Creates a grid of divs for the screen
     for(var x = 0; x < 60; x++) {
         for(var y = 0; y < 40; y++) {
             var unit = $("<div class='unit'></div>");
@@ -9,5 +9,9 @@ $(document).ready(function() {
     // Changes the color of the each unit when the mouse passes over
     $('.unit').mouseenter(function() {
     	$(this).css('background', '#666');
+    });
+
+    $('button').click(function() {
+        $('.unit').css('background', '#fff');
     });
 });
