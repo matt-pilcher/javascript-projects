edbae8caaafbe97bed21d468e3e9cb311780ed6b
mpilcher90 <mpilcher90@gmail.com>
2016-04-13 09:01:46 +0000

    finish recursive fibonacci solution

--- miscellaneous.js
+++ miscellaneous.js
@@ -177,3 +177,15 @@ function fibonacci(num) {
 } 
 
 fibonacci(7); // Output: 13
+
+// Recursive solution
+
+function fibonacci(num) {
+
+    if (num <= 0) { return 0; }
+    if (num <= 2) { return 1; }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+fibonacci(7); // Output: 13
