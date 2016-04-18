cf2095d30e503c637f2ce817521895ddae2449dd
mpilcher90 <mpilcher90@gmail.com>
2016-04-18 10:53:33 +0000

    replace function with variables and cache the dom

--- image-slider/js/script.js
+++ image-slider/js/script.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
-  
-  // Need to set vars for the interval function
-  
+
+	// Config
+	var width = 720,
+			speed = 1000,
+			duration = 3000;
+
+	// Prevent jquery from requerying the dom for the element
+	var $slider = $('#slider');
+			$slideContainer = $slider.find('.slides');
+			$slides = $slideContainer.find('.slide');
+
 	// Set an interval
 	setInterval(function() {
-		$('#slider .slides').animate({'margin-left': '-=720px'}, 1000); // Animate margin-left
-	}, 3000);
-	
-	// On the last slide, go to position 1
-	
+		$slideContainer.animate({'margin-left': '-=' + width}, speed); // Animate margin-left
+	}, duration);
+	// On the last slide, go to position 1. Add a callback
 	// On mouse-enter, pause the animation
 	// Resume the animation after the mouse-leave
-
 })
