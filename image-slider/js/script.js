28fc46d6529413fd574e8bc0fd655465bb3ad7dc
mpilcher90 <mpilcher90@gmail.com>
2016-04-19 13:08:20 +0000

    add pause function when hovering

--- image-slider/js/script.js
+++ image-slider/js/script.js
@@ -3,25 +3,34 @@ $(document).ready(function() {
 	// Config
 	var width = 720,
 			speed = 1000,
-			duration = 3000;
+			duration = 3000,
 			currentSlide = 1;
 
 	// Prevent jquery from requerying the dom for the element
-	var $slider = $('#slider');
-			$slideContainer = $slider.find('.slides');
-			$slides = $slideContainer.find('.slide');
+	var $slider = $('#slider'),
+			$slideContainer = $slider.find('.slides'),
+			$slides = $slideContainer.find('.slide'),
+
+			interval; // Defining this here so I can use in both functions
 
 	// Set an interval
-	setInterval(function() {
-		$slideContainer.animate({'margin-left': '-=' + width}, speed, function() {
-			currentSlide++;
-			if(currentSlide === $slides.length) {
-				currentSlide = 1;
-				$slideContainer.css('margin-left', 0);
-			}
-		});
-	}, duration);
+	function startSlider() {
+		interval = setInterval(function() {
+			$slideContainer.animate({'margin-left': '-=' + width}, speed, function() {
+				currentSlide++;
+				if (currentSlide === $slides.length) {
+					currentSlide = 1;
+					$slideContainer.css('margin-left', 0);
+				}
+			});
+		}, duration);
+	}
 	
-	// On mouse-enter, pause the animation
-	// Resume the animation after the mouse-leave
-})
+	function stopSlider() {
+		clearInterval(interval);
+	}
+
+	$slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+
+	startSlider();
+});
