c5265461effbfed35d8bc3bc49f278c86cdac1d2
mpilcher90 <mpilcher90@gmail.com>
2016-10-11 19:03:18 +0000

    initial commit

---
+++ countdown-timer/count.js
@@ -0,0 +1,13 @@
+/* 
+Var for units
+  current time
+  deadline time
+  seconds/min/hours/days
+
+Function that converts current time to readable
+
+Function that splits the time into seperate objects
+
+Function that displays the time and stops counting after deadline
+
+*/
