47455c88f5985f213e82d529e559b93e799beb37
mpilcher90 <mpilcher90@gmail.com>
2016-10-11 20:17:20 +0000

    finish basic working script. Needs refactoring and styling

--- countdown-timer/count.js
+++ countdown-timer/count.js
@@ -1,13 +1,36 @@
-/* 
-Var for units
-  current time
-  deadline time
-  seconds/min/hours/days
+// Change this date to the prefered deadline
+var date = '2016-11-01';
 
-Function that converts current time to readable
 
-Function that splits the time into seperate objects
+function remainingTime(deadline) {
+  var total = Date.parse(deadline) - Date.parse(new Date()); // Parse method returns milliseconds since 1970-01-01
+  var seconds = Math.floor( (total/1000) % 60 ); // Don't want the total amount of seconds just the remainder. Use modulo
+  var minutes = Math.floor( (total/1000/60) % 60 );
+  var hours = Math.floor( (total/(1000*60*60)) % 24 );
+  var days = Math.floor( total/(1000*60*60*24) );
 
-Function that displays the time and stops counting after deadline
+  return {
+    'total': total,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds,
+  };
+}
 
-*/
+function initializeCounter(id, deadline) {
+  var counterDiv = document.getElementById(id); // This var keeps the function from repeatedly querying the dom
+  var timer = setInterval(function() {
+    var n = remainingTime(deadline);
+    counterDiv.innerHTML = 'Days: ' + n.days + '<br>' +
+                        'Hours: '+ n.hours + '<br>' +
+                        'Minutes: ' + n.minutes + '<br>' +
+                        'Seconds: ' + n.seconds;
+    // Stop the timer after reaching the date
+    if(n.total <= 0) {
+      clearInterval(timer);
+    }
+  },1000); // 1000 milliseconds == 1 second
+}
+
+initializeCounter('counter', date);
