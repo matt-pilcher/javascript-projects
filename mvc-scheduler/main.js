338357264bbf3b54b290381f849d6c83b7a0aab0
mpilcher90 <mpilcher90@gmail.com>
2016-11-13 23:14:39 +0000

    convert list array into an object and add anonymous functions

--- mvc-scheduler/main.js
+++ mvc-scheduler/main.js
@@ -1,27 +1,32 @@
+
+var itinerary = {
+  list: ['item 1','item 2', 'item 3', 'item 4'],
+  
+  displayList: function() {
+    console.log('My List: ', this.list);
+  },
+  
+  addItem: function(item) {
+    this.list.push(item);
+    this.displayList();
+  },
+  
+  changeItem: function(pos, newVal) {
+    this.list[pos] = newVal;
+    this.displayList();
+  },
+  
+  deleteItem: function(pos) {
+    this.list.splice(pos, 1);
+    this.displayList();
+  }
+};
 // It should have a place to store items
-var list = ['item 1','item 2', 'item 3', 'item 4'];
 
 // It should have a way to display a list
-function displayList() {
-  console.log('My List: ', list);
-}
 
 //It should have a way to add an item
-function addItem(item) {
-  list.push(item);
-  displayList();
-}
 
 // It should have a way to change an item
-function changeItem(pos, newVal) {
-  list[pos] = newVal;
-  displayList();
-}
 
 // It should have a way to delete an item
-function deleteItem(pos) {
-  list.splice(pos, 1);
-  displayList();
-}
-
-
