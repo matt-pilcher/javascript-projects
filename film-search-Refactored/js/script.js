68fb42ddcffc0656061e2287a862295de1f8d267
mpilcher90 <mpilcher90@gmail.com>
2018-05-07 19:39:56 +0000

    refactor script to use mvc design pattern

--- film-search-Refactored/js/script.js
+++ film-search-Refactored/js/script.js
@@ -1,16 +1,43 @@
 var movieList = {
 	movies: [],
 
+	init: function() {
+		// request = this.request;
+		// test = this.request;
+		// this.apiCall();
+
+	},
+
 	apiCall: function() {
+		let request, test;
+
 		request = new XMLHttpRequest();
 		request.open('GET', 'https://www.omdbapi.com/?s=lion+king&apikey=6e2e4f34', false);
 		request.send();
 		test = JSON.parse(request.response);
 		this.movies.push(test.Search);
+	}
+
+}
+
+var movieController = {
+	init: function() {
+		movieList.apiCall();
 	},
 
+	getMovies: function() {
+		return movieList.movies;
+	}
+}
+
+var movieView = {
+
 	displayMovies: function() {
-		json = this.movies[0];
+		let json, i, temp
+
+		temp = movieController.getMovies();
+		json = temp[0]
+
 		for (i = 0; i < json.length; i++) { 
     	this.viewTest(json[i].Title, json[i].Year)
 		}
@@ -22,3 +49,6 @@ var movieList = {
 	}
 
 }
+
+
+movieController.init();
